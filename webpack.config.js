3df71ea6c49fa5402743317ce94c975f853cfbcb
Chaudhra <47132229+Chaudhra@users.noreply.github.com>
2020-05-04 16:08:03 -0400

    Setup prod webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
-const path = require('path');
 
-// Expose an object to another file
-module.exports = {
-    entry:'./src/app.js',
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = (env) =>{
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    console.log('env',env);
+    return {
+        entry:'./src/app.js',
     output:{
         path:path.join(__dirname,'public'),
         filename:'bundle.js'
@@ -15,15 +21,35 @@ module.exports = {
             exclude:/node_modules/
         },{
             test: /\.s?css$/,  //The ? makes the s optional
-            use:['style-loader','css-loader','sass-loader']  //Setup css inside of webpack. Use allows us to load multiple loaders in an array
-
+            //use:['style-loader','css-loader','sass-loader']  //Setup css inside of webpack. Use allows us to load multiple loaders in an array
+            use: CSSExtract.extract({
+                use: [{
+                    loader: 'css-loader',
+                    options:{
+                        sourceMap:true
+                    }
+                }, {
+                    loader: 'sass-loader',
+                    options:{
+                        sourceMap:true
+                    }
+                }
+            ]
+            })
         }]
     },
-    devtool:'cheap-module-eval-source-map',
+    plugins:[CSSExtract],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer:{
         contentBase:path.join(__dirname,'public'),
         historyApiFallback:true //For all 404 pages, send back the index.html file
     }
+    };
 };
+
+// Expose an object to another file
+// module.exports = {
+    
+// };
 
 // loader lets you customize how a file is loaded
